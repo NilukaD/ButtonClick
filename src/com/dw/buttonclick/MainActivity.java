3f283b8e0edaf5bc13f32aa412c91093d6ca1cdb
Darshana <darshana@ideahub.lk>
2013-09-10 22:38:34 +0530

    Add button click listner
    Signed-off-by: Darshana <darshana@ideahub.lk>

--- src/com/dw/buttonclick/MainActivity.java
+++ src/com/dw/buttonclick/MainActivity.java
@@ -3,6 +3,7 @@ package com.dw.buttonclick;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -15,6 +16,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         button = (Button)this.findViewById(R.id.button1);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
         
     }
 
