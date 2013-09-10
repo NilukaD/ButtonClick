d04f521c48784a434024abe5c9637066ae420392
Darshana <darshana@ideahub.lk>
2013-09-10 22:30:27 +0530

    Add new button
    Signed-off-by: Darshana <darshana@ideahub.lk>

--- src/com/dw/buttonclick/MainActivity.java
+++ src/com/dw/buttonclick/MainActivity.java
@@ -3,13 +3,19 @@ package com.dw.buttonclick;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        button = (Button)this.findViewById(R.id.button1);
+        
     }
 
     @Override
